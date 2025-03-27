3f58ed17d8ed84fc4f63d69bcff802e94b57400a
Yudha Prawira <111118355+detayuli@users.noreply.github.com>
2025-03-27 11:43:39 +0700

    Revert "Merge branch 'main' into dev"
    This reverts commit 83aa6ce90766d703be7a2bfae6421d2c48a6996d, reversing
    changes made to 7c8d95669ca53ece3c37f2b30ff23c2f14d33bf6.

--- Assets/Scripts/EnemyCookiesMan.cs
+++ Assets/Scripts/EnemyCookiesMan.cs
@@ -1,39 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
-public class EnemyCookiesMan : MonoBehaviour
+public class EnemyCookiesMan : EnemyController
 {
-    [Header("Status")]
-    public float healthPoint = 20f;
-    public float enemyHealthBarFullX = 14.1f;
-    public float attackPoint = 5f;
-    public Transform attackTarget;
-    public SpriteRenderer enemyHealthBar;
-    public GameObject enemy;
-
     [Header("Configuration")]
-    [SerializeField] private float moveSpeed = 2.5f;
     [SerializeField] private float attackMinDistance;
     [SerializeField] private float attackMaxDistance;
     [SerializeField] private float attackTimeMax;
     [SerializeField] private Vector3 shootOffset;
     private float attackTime = 0;
-
+    
     [Header("Graphics and Shooting")]
     [SerializeField] private SpriteRenderer graphic;
     [SerializeField] private GameObject bulletPrefab;
     [SerializeField] private float bulletSpeed;
-
+    
     private Rigidbody2D rb;
-    private float initialHealth;
-
+    
     void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
         rb.constraints = RigidbodyConstraints2D.FreezeRotation;
-        initialHealth = healthPoint; // Simpan nilai awal
     }
 
     void Start()
@@ -41,9 +29,6 @@ public class EnemyCookiesMan : MonoBehaviour
         GameObject target = GameObject.FindGameObjectWithTag("Player");
         if (target)
             attackTarget = target.transform;
-
-        // Health bar penuh di awal
-        UpdateHealthBar();
     }
 
     void Update()
@@ -52,7 +37,7 @@ public class EnemyCookiesMan : MonoBehaviour
         FallDie();
     }
 
-    private void Movement()
+    protected override void Movement()
     {
         if (attackTarget)
         {
@@ -95,47 +80,32 @@ public class EnemyCookiesMan : MonoBehaviour
         }
     }
 
-    public void DamagedBy(float damagePoint)
-    {
-        healthPoint -= damagePoint;
-        healthPoint = Mathf.Clamp(healthPoint, 0, initialHealth);
-
-        UpdateHealthBar();
-
-        if (healthPoint <= 0)
-        {
-            Die();
-        }
-    }
-
-    private void UpdateHealthBar()
-    {
-        if (enemyHealthBar != null)
-        {
-            float healthPercentage = healthPoint / initialHealth;
-            Vector3 currentScale = enemyHealthBar.transform.localScale;
-            currentScale.x = healthPercentage * enemyHealthBarFullX;
-            enemyHealthBar.transform.localScale = currentScale;
-        }
-    }
-
-    private void FallDie()
+    protected override void FallDie()
     {
         if (transform.position.y < -20)
             Die();
     }
 
-    void Die()
+    protected override void Die()
     {
-        ScoreManager.AddScore(10);
+        Debug.Log("Enemy dies");
+        ScoreManager.AddScore(10); 
         Destroy(gameObject);
     }
+
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Ground"))
         {
             rb.constraints = RigidbodyConstraints2D.FreezePositionY | RigidbodyConstraints2D.FreezeRotation;
+        }
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            rb.constraints = RigidbodyConstraints2D.FreezeRotation;
         }
     }
 
